c22673082a65d6ee394c4f7ed341bffc47739a3e
Rudy Brandies <adanb82@gmail.com>
2022-10-07 00:35:34 -0500

    integrated docker hub

--- Startup.cs
+++ Startup.cs
@@ -71,7 +71,11 @@ namespace NewCatalog
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NewCatalog v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+
 
             app.UseRouting();
 
